1a1b127d498cbe35d953ceb7b19a689dbde30fa6
artem <mstrJesm@gmail.com>
2020-08-12 11:57:21 +0300

    finish table resize

---
+++ src/components/table/table.functions.js
@@ -0,0 +1,8 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable eol-last */
+/* eslint-disable linebreak-style */
+/* eslint-disable arrow-parens */
+/* eslint-disable linebreak-style */
+export function shouldResize(event) {
+  return event.target.dataset.resize
+}
